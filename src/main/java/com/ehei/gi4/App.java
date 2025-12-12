18f9a873c4ef1ead696c59c8a8602e09d822af81
Issam Frikal <issamfrikla48@gmail.com>
2025-12-12 15:27:57 +0100

    Produit-management-p

---
+++ src/main/java/com/ehei/gi4/App.java
@@ -0,0 +1,13 @@
+package com.ehei.gi4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
